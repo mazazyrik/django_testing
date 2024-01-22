f9c62fe9c0378f10688cc0a147e8c330b7575b3c
mazazyrik <mazazyrik@yandex.ru>
2024-01-22 17:17:04 +0300

    rev

--- ya_note/notes/tests/test_logic.py
+++ ya_note/notes/tests/test_logic.py
@@ -1,14 +1,29 @@
 from http import HTTPStatus
 
 from django.contrib.auth.models import User
-from django.test import TestCase
+from django.test import Client, TestCase
 from django.urls import reverse
+
+from pytils.translit import slugify
+
 from notes.forms import WARNING
 from notes.models import Note
-from pytils.translit import slugify
+
 TITLE = 'title'
 TEXT = 'text'
 SLUG = 'new-slug'
+
+NOTES_ADD = 'notes:add'
+NOTES_SUCCESS = 'notes:success'
+NOTES_EDIT = 'notes:edit'
+NOTES_DELETE = 'notes:delete'
+NOTES_HOME = 'notes:home'
+NOTES_LIST = 'notes:list'
+NOTES_DETAIL = 'notes:detail'
+
+USERS_LOGOUT = 'users:logout'
+USERS_SIGNUP = 'users:signup'
+USERS_LOGIN = 'users:login'
 
 
 class TestLogic(TestCase):
@@ -22,24 +37,28 @@ class TestLogic(TestCase):
             'slug': SLUG
         }
         cls.user = User.objects.create(username='user')
+        cls.author_client = Client()
+        cls.user_client = Client()
+        cls.author_client.force_login(cls.author)
 
     def test_anonymous_user_cant_create_notes(self):
-        url = reverse('notes:add')
+        url = reverse(NOTES_ADD)
+        notes_count_bef = Note.objects.count()
         response = self.client.post(url, data=self.form_data)
-        login_url = reverse('users:login')
+        login_url = reverse(USERS_LOGIN)
         expected_url = f'{login_url}?next={url}'
         self.assertRedirects(response, expected_url)
         notes_count = Note.objects.count()
-        self.assertEqual(notes_count, 0)
+        self.assertEqual(notes_count, notes_count_bef)
 
     def test_user_can_create_notes(self):
-        url = reverse('notes:add')
-        self.client.force_login(self.author)
-        response = self.client.post(url, data=self.form_data)
-        self.assertRedirects(response, reverse('notes:success'))
+        url = reverse(NOTES_ADD)
+        notes_count_bef = Note.objects.count()
+        response = self.author_client.post(url, data=self.form_data)
+        self.assertRedirects(response, reverse(NOTES_SUCCESS))
         notes_count = Note.objects.count()
-        self.assertEqual(notes_count, 1)
-        notes = Note.objects.get()
+        self.assertEqual(notes_count_bef + 1, notes_count)
+        notes = Note.objects.last()
         self.assertEqual(notes.text, TEXT)
         self.assertEqual(notes.title, TITLE)
         self.assertEqual(notes.slug, SLUG)
@@ -51,22 +70,22 @@ class TestLogic(TestCase):
             text='Текст',
             slug='slug',
             author=self.author)
-        url = reverse('notes:add')
-        self.client.force_login(self.author)
+        url = reverse(NOTES_ADD)
+        notes_count_bef = Note.objects.count()
         self.form_data['slug'] = self.notes.slug
-        response = self.client.post(url, data=self.form_data)
+        response = self.author_client.post(url, data=self.form_data)
         self.assertFormError(response, 'form', 'slug', errors=(
             self.notes.slug + WARNING))
-        self.assertEqual(Note.objects.count(), 1)
+        self.assertEqual(notes_count_bef, Note.objects.count())
 
     def test_empty_slug(self):
-        url = reverse('notes:add')
+        url = reverse(NOTES_ADD)
+        notes_count_bef = Note.objects.count()
         self.form_data.pop('slug')
-        self.client.force_login(self.author)
-        response = self.client.post(url, data=self.form_data)
-        self.assertRedirects(response, reverse('notes:success'))
-        self.assertEqual(Note.objects.count(), 1)
-        new_note = Note.objects.get()
+        response = self.author_client.post(url, data=self.form_data)
+        self.assertRedirects(response, reverse(NOTES_SUCCESS))
+        self.assertEqual(notes_count_bef + 1, Note.objects.count())
+        new_note = Note.objects.last()
         expected_slug = slugify(self.form_data['title'])
         self.assertEqual(new_note.slug, expected_slug)
 
@@ -87,37 +106,40 @@ class TestLogicEditDelete(TestCase):
             slug='slug',
             author=cls.author
         )
+        cls.author_client = Client()
+        cls.user_client = Client()
+        cls.author_client.force_login(cls.author)
+        cls.user_client.force_login(cls.user)
 
     def test_author_can_edit_note(self):
-        self.client.force_login(self.author)
-        url = reverse('notes:edit', args={'slug': self.note.slug})
-        response = self.client.post(url, self.form_data)
-        self.assertRedirects(response, reverse('notes:success'))
+        url = reverse(NOTES_EDIT, args={'slug': self.note.slug})
+        response = self.author_client.post(url, self.form_data)
+        self.assertRedirects(response, reverse(NOTES_SUCCESS))
         self.note.refresh_from_db()
         self.assertEqual(self.note.text, TEXT)
         self.assertEqual(self.note.title, TITLE)
         self.assertEqual(self.note.slug, SLUG)
+        self.assertEqual(self.note.author, self.author)
 
     def test_user_cant_edit_note(self):
-        self.client.force_login(self.user)
-        url = reverse('notes:edit', args={'slug': self.note.slug})
-        response = self.client.post(url, self.form_data)
+        url = reverse(NOTES_EDIT, args={'slug': self.note.slug})
+        response = self.user_client.post(url, self.form_data)
         self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
         note_from_db = Note.objects.get(id=self.note.id)
         self.assertEqual(self.note.text, note_from_db.text)
         self.assertEqual(self.note.title, note_from_db.title)
         self.assertEqual(self.note.slug, note_from_db.slug)
+        self.assertEqual(self.note.author, note_from_db.author)
 
     def test_author_can_delete_note(self):
-        self.client.force_login(self.author)
-        url = reverse('notes:delete', args={'slug': self.note.slug})
-        response = self.client.post(url)
-        self.assertRedirects(response, reverse('notes:success'))
+        url = reverse(NOTES_DELETE, args={'slug': self.note.slug})
+        response = self.author_client.post(url)
+        self.assertRedirects(response, reverse(NOTES_SUCCESS))
         self.assertEqual(Note.objects.count(), 0)
 
     def test_user_cant_delete_note(self):
-        self.client.force_login(self.user)
-        url = reverse('notes:edit', args={'slug': self.note.slug})
-        response = self.client.post(url)
+        url = reverse(NOTES_EDIT, args={'slug': self.note.slug})
+        notes_count_bef = Note.objects.count()
+        response = self.user_client.post(url)
         self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
-        self.assertEqual(Note.objects.count(), 1)
+        self.assertEqual(notes_count_bef, Note.objects.count())
--- ya_note/notes/tests/test_routes.py
+++ ya_note/notes/tests/test_routes.py
@@ -5,6 +5,10 @@ from django.test import TestCase
 from django.urls import reverse
 
 from notes.models import Note
+from notes.tests.test_logic import (NOTES_ADD, NOTES_DELETE, NOTES_DETAIL,
+                                    NOTES_EDIT, NOTES_HOME, NOTES_LIST,
+                                    NOTES_SUCCESS, USERS_LOGIN, USERS_LOGOUT,
+                                    USERS_SIGNUP)
 
 User = get_user_model()
 
@@ -27,10 +31,10 @@ class TestRoutes(TestCase):
         )
 
     def test_pages(self):
-        urls = ('notes:home',
-                'users:login',
-                'users:logout',
-                'users:signup')
+        urls = (NOTES_HOME,
+                USERS_LOGIN,
+                USERS_LOGOUT,
+                USERS_SIGNUP)
         for reverse_url in urls:
             with self.subTest(reverse_url=reverse_url):
                 url = reverse(reverse_url)
@@ -39,9 +43,9 @@ class TestRoutes(TestCase):
 
     def test_list_success_add_for_loginus(self):
         urls = (
-            ('notes:list'),
-            ('notes:success'),
-            ('notes:add'),
+            (NOTES_LIST),
+            (NOTES_SUCCESS),
+            (NOTES_ADD),
         )
         self.client.force_login(self.author)
         for reverse_url in urls:
@@ -55,9 +59,26 @@ class TestRoutes(TestCase):
             (self.author, HTTPStatus.OK),
         )
         urls = (
-            ('notes:detail'),
-            ('notes:edit'),
-            ('notes:delete'),
+            (NOTES_DETAIL),
+            (NOTES_EDIT),
+            (NOTES_DELETE),
+        )
+        for user, status in users_statuses:
+            self.client.force_login(user)
+            for reverse_urls in urls:
+                with self.subTest(user=user, reverse_urls=reverse_urls):
+                    url = reverse(reverse_urls, args=(self.notes.slug))
+                    response = self.client.get(url)
+                    self.assertEqual(response.status_code, status)
+
+    def test_detail_edit_delete_for_user(self):
+        users_statuses = (
+            (self.user, HTTPStatus.NOT_FOUND),
+        )
+        urls = (
+            (NOTES_DETAIL),
+            (NOTES_EDIT),
+            (NOTES_DELETE),
         )
         for user, status in users_statuses:
             self.client.force_login(user)
@@ -69,16 +90,16 @@ class TestRoutes(TestCase):
 
     def test_redirect_for_anonymous(self):
         urls = (
-            ('notes:edit', (self.notes.slug)),
-            ('notes:delete', (self.notes.slug)),
-            ('notes:detail', (self.notes.slug)),
-            ('notes:list', None),
-            ('notes:success', None),
-            ('notes:add', None),
+            (NOTES_EDIT, (self.notes.slug)),
+            (NOTES_DELETE, (self.notes.slug)),
+            (NOTES_DETAIL, (self.notes.slug)),
+            (NOTES_LIST, None),
+            (NOTES_SUCCESS, None),
+            (NOTES_ADD, None),
         )
         for reverse_url, args in urls:
             with self.subTest(reverse_url=reverse_url, args=args):
-                login_url = reverse('users:login')
+                login_url = reverse(USERS_LOGIN)
                 url = reverse(reverse_url, args=args)
                 next_url = f'{login_url}?next={url}'
                 response = self.client.get(url)
